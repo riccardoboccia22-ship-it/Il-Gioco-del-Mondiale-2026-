dd894dcf86a7e3df96a8e4b6dc8f0f35703ef134
riccardoboccia22-ship-it <riccardoboccia22@gmail.com>
2026-05-07 15:12:15 +0200

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      // Questo permette di completare il build anche se ci sono 
-      // avvisi sugli apostrofi o sulle immagini <img>
       ignoreDuringBuilds: true,
     },
     typescript: {
-      // Ignora anche eventuali errori di "tipo" per ora
       ignoreBuildErrors: true,
     },
   }
